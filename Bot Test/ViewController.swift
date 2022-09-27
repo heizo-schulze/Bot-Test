51a0113f367a956e4c740fecaffffbf83a93bc48
heizo-schulze <git@hzo-film.de>
2022-09-27 10:07:20 +0200

    Initial Commit

---
+++ Bot Test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Bot Test
+//
+//  Created by Heizo Schulze on 27.09.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
